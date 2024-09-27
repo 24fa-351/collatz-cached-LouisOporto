579bcc09fec6913f23e9f466c3295d7aff209887
Louis Oporto <louisoporto042@csu.fullerton.edu>
2024-09-27 01:41:06 -0700

    Setup goals to be completed

--- collatz.c
+++ collatz.c
@@ -5,6 +5,17 @@
 int collatzConject(int);
 int collatzWrapper(int);
 
+/* TODO
+1. Build a wrapper around the core function
+2. Program arguments that specify cache policy (none, LRU, x) and cache size
+3. Implement LRU cache last CWID is 8 so RR (Random Replacement)
+4. Report Cache hit % in output.csv
+5. Determine smallest cache size for (N=10000, MIN=1, MAX=500000) 30% cache hit
+Or multiply N by 5
+6. Do this for both cache types
+7. Extra Credit: Implement ARC
+*/
+
 int main(int argc, char* argv[]) {
     // Three arguments values
     // N = number of values
@@ -15,21 +26,22 @@ int main(int argc, char* argv[]) {
     int MIN = atoi(argv[2]);
     int MAX = atoi(argv[3]);
 
-    FILE *fpt;
+    // Output to .csv file
+    FILE* fpt;
     fpt = fopen("output.csv", "w+");
     fprintf(fpt, "Number, Steps\n");
 
     // Generate N number of random values
     srand(time(NULL));
-    for(int iter = 0; iter < N; iter++) {
+    for (int iter = 0; iter < N; iter++) {
         int randNum = 0;
         int counter = 0;
         do {
             randNum = rand() % MAX;
-        } while(randNum < MIN);
+        } while (randNum < MIN);
 
         int temp = randNum;
-        while(temp != 1 ) {
+        while (temp != 1) {
             temp = collatzConject(temp);
             counter++;
         }
@@ -39,12 +51,16 @@ int main(int argc, char* argv[]) {
         fprintf(fpt, "%d, %d\n", randNum, counter);
     }
 
+    printf("Output saved to output.csv");
+
     return 0;
 }
 
 int collatzConject(int x) {
-  if(x % 2 == 0) return x / 2;
-  else return 3 * x + 1;
+    if (x % 2 == 0)
+        return x / 2;
+    else
+        return 3 * x + 1;
 }
 
 // // Goal
